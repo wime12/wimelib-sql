6e5d266ce3b6415552383ccd6286aaca727b3478
wime12 <wilfried.meindl@gmail.com>
2012-09-10 16:48:32 +0300

    Add exec macro to sqlite-test.lisp

--- sqlite-test.lisp
+++ sqlite-test.lisp
@@ -9,15 +9,17 @@
 (defun open-db (name)
   (setf *db* (wimelib-sqlite3:sqlite3-open name)))
 
+(defmacro exec (sexp &optional fun)
+  `(wimelib-sqlite3:sqlite3-exec *db* (ssql ,sexp) ,fun))
+
 (defmacro query (sexp)
   `(let ((result nil))
-     (wimelib-sqlite3:sqlite3-exec *db* (ssql ,sexp)
-				   (lambda (res)
-				     (when (not result)
-				       (push (wimelib-sqlite3:sqlite3-column-names
-					      res) result))
-				     (push (wimelib-sqlite3:sqlite3-row-values res)
-					   result)))
+     (exec ,sexp (lambda (res)
+		   (when (not result)
+		     (push (wimelib-sqlite3:sqlite3-column-names
+			    res) result))
+		   (push (wimelib-sqlite3:sqlite3-row-values res)
+			 result)))
      (let ((res (nreverse result)))
        (values (cdr res)
 	       (car res)))))
